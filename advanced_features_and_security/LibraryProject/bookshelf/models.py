ec9120a959abe3884e78e5de8da00c44389dd82a
Priscakoech <priscakoech448@gmail.com>
2024-11-27 17:31:51 +0300

    Add custom user

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -6,4 +6,22 @@ class Book(models.Model):
     author = models.CharField(max_length=100)
     publication_year = models.IntegerField()
     
+ class CustomUser(AbstractUser):
+    email = models.EmailField(unique=True, blank=False)
+    date_of_birth = models.DateField(null=True, blank=True)
+    profile_photo = models.ImageField(null=True, blank=True)
     
+    objects = CustomUserManager()
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+    
+    def __str__(self):
+        return self.email
+    
+from django.conf import settings
+from django.db import models
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    bio = models.TextField()
+   
