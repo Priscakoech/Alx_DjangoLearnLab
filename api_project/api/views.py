9f47385317aa16ed6fedc879ff8a21bf86000491
Priscakoech <priscakoech448@gmail.com>
2024-12-06 16:25:12 +0300

    Add routers and viewsets

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -3,6 +3,7 @@ from rest_framework import generics
 from .models import Book
 from .serializers import BookSerializer
 from rest_framework.viewsets import ModelViewSet
+from rest_framework import viewsets
 
 # Create your views here.
 
