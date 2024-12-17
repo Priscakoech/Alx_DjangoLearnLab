5a101c95cc47f64a62d91c82edaeef1a9cfad25f
Priscakoech <priscakoech448@gmail.com>
2024-12-17 23:14:46 +0300

    Add

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from .models import Book
 from .serializers import BookSerializer
 from rest_framework import generics
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 
 # Create your views here.
 # ListView: Retrieve all books
