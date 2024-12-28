946ba2e50e6e7b4c7b7669a8a23ce82ebe50d274
Priscakoech <priscakoech448@gmail.com>
2024-12-28 17:43:08 +0300

    initial setup and project configuration

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -78,6 +78,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+        'USER': 'prisca'
+        'PASSWORD': 'prisca@2022'
+        'HOST': 'localhost'
+        'PORT': '5432'
     }
 }
 
