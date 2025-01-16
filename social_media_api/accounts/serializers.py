d2701e5d7a9f53375862681133db4f8f14b28994
Priscakoech <priscakoech448@gmail.com>
2025-01-16 13:30:37 +0300

    add customuser and authentication

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from rest_framework.authtoken.models import Token  # Token import for authentication
 from django.contrib.auth import get_user_model
 from rest_framework.validators import UniqueValidator
 from django.contrib.auth.password_validation import validate_password
@@ -10,18 +11,53 @@ User = get_user_model()
 class RegisterSerializer(serializers.ModelSerializer):
     # Fields for password and password confirmation
     password = serializers.CharField(
-        write_only=True, 
-        required=True, 
+        write_only=True,
+        required=True,
         validators=[validate_password],
         style={'input_type': 'password'}
     )
     password2 = serializers.CharField(
-        write_only=True, 
-        required=True, 
+        write_only=True,
+        required=True,
         style={'input_type': 'password'}
     )
 
     class Meta:
         model = User
-        fields = ['username', 'email', 'password', 'password2', '
-        ]
+        fields = ['username', 'email', 'password', 'password2', 'bio', 'profile_picture']
+
+    def validate(self, attrs):
+        # Ensure passwords match
+        if attrs['password'] != attrs['password2']:
+            raise serializers.ValidationError({"password": "Passwords do not match."})
+        return attrs
+
+    def create(self, validated_data):
+        # Remove password2 from validated_data
+        validated_data.pop('password2', None)
+
+        # Use get_user_model() to create a new user
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            password=validated_data['password'],
+            bio=validated_data.get('bio', ''),
+            profile_picture=validated_data.get('profile_picture', None)
+        )
+
+        # Generate a token for the new user
+        Token.objects.create(user=user)
+
+        return user
+
+
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField(
+        required=True,
+        style={'input_type': 'text'}
+    )
+    password = serializers.CharField(
+        write_only=True,
+        required=True,
+        style={'input_type': 'password'}
+    )
