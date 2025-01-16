c2083cbbc7ca371fadfcd546fa5a16c6c70cb3b6
Priscakoech <priscakoech448@gmail.com>
2025-01-16 13:24:47 +0300

    Add authentication and custom user

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,34 +1,28 @@
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
-from rest_framework.validators import UniqueValidator
-from django.contrib.auth.password_validation import validate_password
 from rest_framework.authtoken.models import Token
 
+# Dynamically fetch the custom user model
 User = get_user_model()
 
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ['id', 'username', 'bio', 'profile_picture', 'followers']
 
-class RegisterSerializer(serializers.ModelSerializer):
-    email = serializers.EmailField(
-        required=True,
-        validators=[UniqueValidator(queryset=User.objects.all())]
-    )
-    password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
-    password2 = serializers.CharField(write_only=True, required=True)
+class UserRegistrationSerializer(serializers.ModelSerializer):
+    # Define password explicitly for hashing and write-only usage
+    password = serializers.CharField(write_only=True, required=True, style={'input_type': 'password'})
 
     class Meta:
         model = User
-        fields = ('username', 'email', 'password', 'password2', 'bio', 'profile_picture')
-
-    def validate(self, attrs):
-        if attrs['password'] != attrs['password2']:
-            raise serializers.ValidationError({"password": "Password fields didn't match."})
-        return attrs
+        fields = ['id', 'username', 'email', 'password', 'bio', 'profile_picture']
 
     def create(self, validated_data):
-        validated_data.pop('password2')
-        user = User.objects.create_user(**validated_data)
+        # Create a new user securely
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            password=validated_data['password'],
+            email=validated_data.get('email', ''),
+            bio=validated_data.get('bio', ''),
+            profile_picture=validated_data.get('profile_picture', None)
+        )
+        # Automatically create and return an authentication token for the new user
+        Token.objects.create(user=user)
         return user
