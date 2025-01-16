7d4a2fca49fd5acb32d9bb9ce95bec0f01a4f444
Priscakoech <priscakoech448@gmail.com>
2025-01-16 13:32:52 +0300

    Add custonuser and authentication

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,7 +1,5 @@
 from rest_framework import serializers
-from rest_framework.authtoken.models import Token  # Token import for authentication
 from django.contrib.auth import get_user_model
-from rest_framework.validators import UniqueValidator
 from django.contrib.auth.password_validation import validate_password
 
 # Get the custom user model
@@ -24,7 +22,7 @@ class RegisterSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ['username', 'email', 'password', 'password2', 'bio', 'profile_picture']
+        fields = ['username', 'email', 'password', 'password2']
 
     def validate(self, attrs):
         # Ensure passwords match
@@ -36,17 +34,12 @@ class RegisterSerializer(serializers.ModelSerializer):
         # Remove password2 from validated_data
         validated_data.pop('password2', None)
 
-        # Use get_user_model() to create a new user
+        # Use get_user_model().objects.create_user to create a new user
         user = User.objects.create_user(
             username=validated_data['username'],
             email=validated_data['email'],
-            password=validated_data['password'],
-            bio=validated_data.get('bio', ''),
-            profile_picture=validated_data.get('profile_picture', None)
+            password=validated_data['password']
         )
-
-        # Generate a token for the new user
-        Token.objects.create(user=user)
 
         return user
 
